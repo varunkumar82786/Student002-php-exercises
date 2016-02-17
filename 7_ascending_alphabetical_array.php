13a0a977591e9c468b7fd7d70ede1c4b7d319e9d
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-17 15:14:18 -0500

    created example on alphabetically arranging elements of an array

---
+++ 7_ascending_alphabetical_array.php
@@ -0,0 +1,16 @@
+<?php
+/*this example shows how to 
+  arrange an array in ascending arraya and printing them.*/
+$pcgames = array("Devil May Cry","Prince Of Persia","Assassin's Creed","Gears Of War");
+sort($pcgames);
+for($x=0;$x<count($pcgames);$x++){
+	echo $pcgames[$x]."<br>";
+}
+/*Output:
+Assassin's Creed
+Devil May Cry
+Gears Of War
+Prince Of Persia
+ */
+
+?>
