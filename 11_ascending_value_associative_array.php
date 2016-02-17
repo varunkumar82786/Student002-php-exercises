26f14ce83303a457425bb3875a0de77b7483842b
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-17 15:41:56 -0500

    created example on ascending arrangement of associative array

---
+++ 11_ascending_value_associative_array.php
@@ -0,0 +1,17 @@
+<?php
+/*this example shows how to print the elements of an
+ * associative array in ascending order according to value.
+ */
+
+$sides = array("triangle" => "3","hexagon" => "6","rectangle" => "4","pentagon" => "5");
+asort($sides);
+foreach($sides as $x => $x_value){
+	echo $x." = ".$x_value."<br>";
+}
+/*Output:
+triangle = 3
+rectangle = 4
+pentagon = 5
+hexagon = 6
+ */
+?>
