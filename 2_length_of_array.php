e4681942d7cfab3809264ede2e22833a9975f865
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-17 13:53:41 -0500

    created example on length of array

---
+++ 2_length_of_array.php
@@ -0,0 +1,8 @@
+<?php
+/*This example shows how to find the length of the array*/
+$games = array("Football","Basketball","Volleyball","Cricket","Tennis");
+echo "The length of the array is "; 
+echo count($games);
+
+/*Output:The length of the array is 5 */
+?>
