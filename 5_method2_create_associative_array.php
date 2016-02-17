96d6382a169343d34a749e2ad74a0ed43749ad3a
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-17 14:26:04 -0500

    created an example on second method of creating associative array

---
+++ 5_method2_create_associative_array.php
@@ -0,0 +1,27 @@
+<?php
+
+/*this example shows the second method of creating an
+ * associative array.
+*/
+
+$class_strength['A'] = "30";
+$class_strength['B'] = "40";
+$class_strength['C'] = "35";
+$class_strength['D'] = "25";
+echo "the strength of class A is ";
+echo $class_strength['A'];
+echo "<br>";
+echo "the strength of class B is ";
+echo $class_strength['B'];
+echo "<br>";
+echo "the strength of class C is ";
+echo $class_strength['C'];
+echo "<br>";
+echo "the strength of class D is ";
+echo $class_strength['D'];
+echo "<br>";
+/*the strength of class A is 30
+ the strength of class B is 40
+ the strength of class C is 35
+ the strength of class D is 25*/
+?>
