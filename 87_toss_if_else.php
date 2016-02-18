8e5d2b1d62f4a066ad48b32f6040a5696f560ac6
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 18:46:28 -0500

    created example on toss using if else statement

---
+++ 87_toss_if_else.php
@@ -0,0 +1,11 @@
+<?php
+/*this example uses if else statement*/
+$toss = "tails";
+if($toss == "heads"){
+	echo "I will get to choose.";
+}
+else{
+	echo "You get to choose.";
+}
+//Output: You get to choose.
+?>
