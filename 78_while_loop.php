34b6a7b85135b7c7f4743481d228edc0a3333777
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 17:34:33 -0500

    created example on while loop

---
+++ 78_while_loop.php
@@ -0,0 +1,21 @@
+<?php
+/*this is an example on while loop*/
+$x = 0;
+while($x<=100){
+	echo "You have covered "."<b>$x</b>"." meters.<br>";
+	$x+=10;
+}
+/*Output:
+You have covered 0 meters.
+You have covered 10 meters.
+You have covered 20 meters.
+You have covered 30 meters.
+You have covered 40 meters.
+You have covered 50 meters.
+You have covered 60 meters.
+You have covered 70 meters.
+You have covered 80 meters.
+You have covered 90 meters.
+You have covered 100 meters.
+ */
+?>
