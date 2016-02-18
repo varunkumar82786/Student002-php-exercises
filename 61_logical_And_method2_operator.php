df2baf03310f93cf7f6908551099f38a0853d13f
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 16:01:50 -0500

    created example on logical And operator method 2

---
+++ 61_logical_And_method2_operator.php
@@ -0,0 +1,10 @@
+<?php
+/*this is an example on logical And operator method 2*/
+$a = "movie1";
+$b = "movie2";
+
+if($a == "movie1" & $b == "movie2"){
+	echo "Lets go to the movies.";
+}
+//Output: Lets go to the movies.
+?>
