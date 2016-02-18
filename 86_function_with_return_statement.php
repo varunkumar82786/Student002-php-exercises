d0ed6f583cb2f41374e4c97f7fd9665aa6cde3e4
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 18:39:44 -0500

    created example on function with return statement

---
+++ 86_function_with_return_statement.php
@@ -0,0 +1,13 @@
+<?php
+/*this is an example of function with return statement*/
+function sum($x,$y){
+	 $z = $x+$y;
+	 return $z;
+}
+echo "2 + 3 = ".sum(2,3)."<br>";
+echo "3 + 5 = ".sum(3,5)."<br>";
+/*Output
+ 2 + 3 = 5
+ 3 + 5 = 8
+ */
+?>
