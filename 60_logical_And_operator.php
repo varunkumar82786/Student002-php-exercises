8fb4bec3657b1d94e6947f78fc7f8b0183d42fff
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 16:00:03 -0500

    created example on logical And operator method 1

---
+++ 60_logical_And_operator.php
@@ -0,0 +1,10 @@
+<?php
+/*this is an example on logical And operator method 1*/
+$a = "movie1";
+$b = "movie2";
+
+if($a == "movie1" and $b == "movie2"){
+	echo "Lets go to the movies.";
+}
+//Output: Lets go to the movies
+?>
