8c72d34ee68f834c5c9ed38056d800c731808059
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 17:07:55 -0500

    created example on if else statement

---
+++ 75_If_else_statement.php
@@ -0,0 +1,11 @@
+<?php
+/* this is an example on if else statement*/
+$bal = 1000;
+if($bal<500){
+	echo "Your balance is low.";
+}
+else{
+	echo "You have enough balance.";
+}
+//Output: You have enough balance.
+?>
