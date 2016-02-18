f2a121295e8fdad053d585fc0b348a66c7ca9edf
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 16:33:12 -0500

    created example on array union operator

---
+++ 68_array_union_operator.php
@@ -0,0 +1,15 @@
+<?php
+/*this is an example on array union operator*/
+$x = array("a" => 1,"b" => 2,"c" => 3);
+$y = array("d" => 4,"e" => 5,"f" => 6);
+var_dump($x);
+echo "<br>";
+var_dump($y);
+echo "<br>";
+var_dump($x+$y);
+/*Output:
+array(3) { ["a"]=> int(1) ["b"]=> int(2) ["c"]=> int(3) } 
+array(3) { ["d"]=> int(4) ["e"]=> int(5) ["f"]=> int(6) } 
+array(6) { ["a"]=> int(1) ["b"]=> int(2) ["c"]=> int(3) ["d"]=> int(4) ["e"]=> int(5) ["f"]=> int(6) }
+ */
+?>
