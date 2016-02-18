6bb66d57617bf2ca9c9552c6c73cc10fedc33e84
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 16:07:17 -0500

    created example on logical Or operator method 2

---
+++ 63_logical_Or2_operator.php
@@ -0,0 +1,10 @@
+<?php
+/*this is an example on logical Or operator method 2*/
+$a = "movie";
+$b = "noMovie";
+
+if($a == "movie" || $b == "movie2"){
+	echo "Lets go to the movies.";
+}
+//Output: Lets go to the movies.
+?>
