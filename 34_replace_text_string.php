5e1bb48c7de24e4ab1a55e434d1153306fd0db8a
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 14:21:04 -0500

    created example to replace text in a string

---
+++ 34_replace_text_string.php
@@ -0,0 +1,7 @@
+<?php
+/*this is example to replace text in a string*/
+echo str_replace("sea","cee","She sells sea shells on ths sea shore.");
+/*Output:
+ She sells cee shells on ths cee shore.
+*/
+?>
