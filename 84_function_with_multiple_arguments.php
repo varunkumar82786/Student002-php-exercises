09d4dfe72a1440262760fdb795d2818be5de132f
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 18:24:54 -0500

    created example on function with multiple arguments

---
+++ 84_function_with_multiple_arguments.php
@@ -0,0 +1,14 @@
+<?php
+/*this is an example of function with multiple arguments*/
+function sum($x,$y){
+	echo $x+$y."<br>";
+}
+echo "2 + 3 = ";
+sum(2,3);
+echo "3 + 5 = ";
+sum(3,5);
+/*Output
+2 + 3 = 5
+3 + 5 = 8
+*/
+?>
