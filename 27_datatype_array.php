c883269dcd454948bff3bd5d722294d0ee33b09c
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 13:57:06 -0500

    created example on array datatype

--- 27_datatype_array.php
+++ 27_datatype_array.php
@@ -1,5 +1,5 @@
 <?php
-/* this is an example on array datatype*/
+/*this is an example on array datatype*/
 $continents = array("Asia","Africa","Australia","North America","South America","Eurpoe","Antartica");
 for($x=0;$x<count($continents);$x++){
 	echo $continents[$x]."<br>";
