2e7352ec1ddfe934ab867775fda20de1b349230a
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 16:39:17 -0500

    created example on array identiy operator

---
+++ 70_array_identity_operator.php
@@ -0,0 +1,15 @@
+<?php
+/*this is an example on array identity operator*/
+$x = array("a" => 1,"b" => 2,"c" => 3);
+$y = array("a" => 1,"b" => 2,"c" => 3);
+var_dump($x);
+echo "<br>";
+var_dump($y);
+echo "<br>";
+var_dump($x===$y);
+/*Output:
+array(3) { ["a"]=> int(1) ["b"]=> int(2) ["c"]=> int(3) } 
+array(3) { ["a"]=> int(1) ["b"]=> int(2) ["c"]=> int(3) } 
+bool(true)
+ */
+?>
