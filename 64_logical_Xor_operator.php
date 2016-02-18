e55e27575ba3c64f21a71a6cfb9d972fe72d6620
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 16:11:02 -0500

    created example on logical Xor operator

---
+++ 64_logical_Xor_operator.php
@@ -0,0 +1,13 @@
+<?php
+/*this is an example on logical Xor operator*/
+$a = "movie";
+$b = "noMovie";
+
+if($a == "movie" Xor $b == "noMovie"){
+	echo "Lets go to the movies.";
+}
+else{
+	echo "Lets not go to the movies.";
+}
+//Output: Lets not go to the movies.
+?>
