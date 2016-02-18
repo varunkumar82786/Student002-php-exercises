8d18ce5631420f93da39e7d929fc3d16b0e9f041
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 17:53:41 -0500

    created example on for loop

---
+++ 80_for_loop.php
@@ -0,0 +1,20 @@
+<?php
+/*this is an example on for loop*/
+
+for($x=10;$x>=0;$x--){
+	echo "<b>$x</b>"." seconds to launch.<br>";
+}
+/*Output:
+10 seconds to launch.
+9 seconds to launch.
+8 seconds to launch.
+7 seconds to launch.
+6 seconds to launch.
+5 seconds to launch.
+4 seconds to launch.
+3 seconds to launch.
+2 seconds to launch.
+1 seconds to launch.
+0 seconds to launch.
+*/
+?>
