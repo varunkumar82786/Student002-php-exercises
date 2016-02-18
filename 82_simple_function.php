442045979fc97fbc803a24758b2edabae4ac7400
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 18:11:21 -0500

    created example on a simple function

---
+++ 82_simple_function.php
@@ -0,0 +1,9 @@
+<?php
+/*this is an example on a simpe fucntion*/
+function song(){
+	echo "Welcome to My house.";
+}
+echo "The New song by Flo Rida is ";
+song();
+//Output: The New song by Flo Rida is Welcome to My house.
+?>
