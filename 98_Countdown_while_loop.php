4cd34828ba059900e173de79bbfc73d568d5ead2
VarunKumar <varunkumarmopidevi@gmail.com>
2016-02-18 20:52:14 -0500

    created example to display countdown using while loop

---
+++ 98_Countdown_while_loop.php
@@ -0,0 +1,22 @@
+<?php
+/*this is an example to display countdown using while loop*/
+$x=10;
+while($x>=1){
+	echo $x." seconds to impact.<br>";
+	$x--;
+}
+echo "collison occred.";
+/*Output:
+10 seconds to impact.
+9 seconds to impact.
+8 seconds to impact.
+7 seconds to impact.
+6 seconds to impact.
+5 seconds to impact.
+4 seconds to impact.
+3 seconds to impact.
+2 seconds to impact.
+1 seconds to impact.
+collison occred.
+ */
+?>
